e1f8b4b94c09cd2a3f7dd2b62698f9be29ecec1e
Brady Mavetz <b.mavetz@outlook.com>
2022-05-13 00:23:54 -0500

    changing name of handlebar for heroku route

--- controllers/homeRoutes/post.js
+++ controllers/homeRoutes/post.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/',(req,res)=>{
-    res.render('newpost', {loggedIn:req.session.loggedIn});
+    res.render('createpost', {loggedIn:req.session.loggedIn});
 })
 
 module.exports = router;
