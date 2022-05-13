b0a564af995dfe38f8925435f83963bdb389848c
Brady Mavetz <b.mavetz@outlook.com>
2022-05-13 00:00:49 -0500

    post routes changed

--- controllers/homeRoutes/post.js
+++ controllers/homeRoutes/post.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/',(req,res)=>{
-    res.render('newPost', {loggedIn:req.session.loggedIn});
+    res.render('newpost', {loggedIn:req.session.loggedIn});
 })
 
 module.exports = router;
